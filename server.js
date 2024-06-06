eaad350583ceff54438d3e91178adb36ff6d3b17
RezaBagusS <rezabahussaputra036@gmail.com>
2024-06-06 15:44:00 +0700

    [REMOVE] clear api ML

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const path = require('path');
-const tf = require('@tensorflow/tfjs-node');
 require("dotenv").config();
 const app = express();
 const port = 3000;
 const upload = multer();
-
-// class L2 {
-
-//     static className = 'L2';
-
-//     constructor(config) {
-//        return tf.regularizers.l1l2(config)
-//     }
-// }
-// tf.serialization.registerClass(L2);
 
 // 📦 Route
 const authRouter = require('./src/handlers/auth/auth.controller');
@@ -36,10 +24,6 @@ app.get("/api/v1", (_req, res) => {
     res.status(200).json({ message: "This Api for Suarga App version 1" });
 });
 
-app.get("/TestModel", (_req, res) => {
-    res.status(200).json({ message: "Model Testing" });
-});
-
 // 🚀 API ROUTE
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/assessment', assessmentRouter);
@@ -53,30 +37,6 @@ app.post('/uploadGambar', upload.single("image"), (req, res) => {
         data: req.file
     });
 });
-
-// app.get('/getMyModel', async (req, res) => {
-//     const filePath = path.resolve(__dirname, './src/models/models.json');
-//     return res.sendFile(filePath);
-// });
-
-// app.get('/predict', async (req, res) => {
-//     try {
-//         const model = await tf.loadLayersModel('http://localhost:3000/getMyModel');
-//         // const model = await tf.loadLayersModel('https://storage.googleapis.com/pcp-detection/model.json');
-//         // const model = await tf.loadLayersModel('https://raw.githubusercontent.com/sofit-c23-ps233/SoFit-MachineLearning/main/model_ml/model.json');
-
-//         res.json({
-//             message: "Predict Berhasil",
-//             data: model.summary()
-//         })
-//     } catch (error) {
-//         res.json({
-//             message: "Predict Gagal",
-//             data: error.message
-//         });
-//     }
-
-// });
 
 // 💨 Not Found Route
 app.get("*", (_req, res) => {
