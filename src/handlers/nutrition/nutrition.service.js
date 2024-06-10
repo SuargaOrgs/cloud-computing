997dfe20aa0705a31dfe2ea6404ac6984e66e226
Yohanneslex <50076171+yohanneslex@users.noreply.github.com>
2024-06-10 20:25:38 +0700

    [ADD] API Post and GET

--- src/handlers/nutrition/nutrition.service.js
+++ src/handlers/nutrition/nutrition.service.js
@@ -1,37 +1,29 @@
-const { formatDateText } = require('../../helpers/formatDate');
 const prisma = require('../../helpers/prisma');
 const { verifyToken } = require('../../middlewares/jwt');
+const { formatDateText } = require('../../helpers/formatDate');
 
 const getData = async (data) => {
+    const { token } = data;
 
-    const { token } = data
-
-    const verify = verifyToken(token)
+    const verify = verifyToken(token);
 
     if (!verify) {
         return {
             status: 401,
             error: true,
             message: "Session has expired, please login again!!"
-        }
+        };
     }
 
     try {
-
         const nutrition = await prisma.imageNutrition.findMany({
-            where: {
-                idUser: verify.idUser
-            },
+            where: { idUser: verify.idUser },
             select: {
                 id: true,
                 idUser: true,
                 idMakanan: true,
                 gambar: true,
-                makanan: {
-                    select: {
-                        namaMakanan: true
-                    }
-                },
+                makanan: { select: { namaMakanan: true } },
                 waktuMakan: true,
                 created_at: true
             }
@@ -43,20 +35,18 @@ const getData = async (data) => {
                 error: false,
                 message: "Data nutrition not found",
                 data: []
-            }
+            };
         }
 
-        const formatData = nutrition.map((item) => {
-            return {
-                id: item.id,
-                idUser: item.idUser,
-                gambar: item.gambar,
-                namaMakanan: item.makanan.namaMakanan,
-                keterangan: item.waktuMakan + " | " + formatDateText(item.created_at),
-                created_at: item.created_at
-            }
-        }).sort((a, b) => b.created_at - a.created_at);
-        
+        const formatData = nutrition.map((item) => ({
+            id: item.id,
+            idUser: item.idUser,
+            gambar: item.gambar,
+            namaMakanan: item.makanan.namaMakanan,
+            keterangan: item.waktuMakan + " | " + formatDateText(item.created_at),
+            created_at: item.created_at
+        })).sort((a, b) => b.created_at - a.created_at);
+
         return {
             status: 200,
             error: false,
@@ -71,29 +61,24 @@ const getData = async (data) => {
             data: error.message
         };
     }
-}
+};
 
 const detailData = async (data) => {
-
     const { token } = data;
 
-    const verify = verifyToken(token)
+    const verify = verifyToken(token);
 
     if (!verify) {
         return {
             status: 401,
             error: true,
             message: "Session has expired, please login again!!"
-        }
-        
+        };
     }
 
     try {
-
         const nutrition = await prisma.imageNutrition.findMany({
-            where: {
-                idUser: verify.idUser
-            },
+            where: { idUser: verify.idUser },
             select: {
                 id: true,
                 idUser: true,
@@ -119,24 +104,22 @@ const detailData = async (data) => {
                 error: false,
                 message: "Detail nutrition not found",
                 data: []
-            }
+            };
         }
 
-        const formatData = nutrition.map((item) => {
-            return {
-                id: item.id,
-                idUser: item.idUser,
-                idMakanan: item.idMakanan,
-                gambar: item.gambar,
-                waktuMakan: item.waktuMakan,
-                namaMakanan: item.makanan.namaMakanan,
-                karbohidrat: item.makanan.karbohidrat,
-                lemak: item.makanan.lemak,
-                protein: item.makanan.protein,
-                update_at: item.updated_at,
-                created_at: item.created_at
-            }
-        }).sort((a, b) => b.created_at - a.created_at);
+        const formatData = nutrition.map((item) => ({
+            id: item.id,
+            idUser: item.idUser,
+            idMakanan: item.idMakanan,
+            gambar: item.gambar,
+            waktuMakan: item.waktuMakan,
+            namaMakanan: item.makanan.namaMakanan,
+            karbohidrat: item.makanan.karbohidrat,
+            lemak: item.makanan.lemak,
+            protein: item.makanan.protein,
+            update_at: item.updated_at,
+            created_at: item.created_at
+        })).sort((a, b) => b.created_at - a.created_at);
 
         return {
             status: 200,
@@ -152,10 +135,93 @@ const detailData = async (data) => {
             data: error.message
         };
     }
+};
 
-}
+const saveImageNutrition = async (data) => {
+    const { token, linkGambar, namaAktivitas, waktuMakan, idMakanan, porsi } = data;
+
+    const verify = verifyToken(token);
+
+    if (!verify) {
+        return {
+            status: 401,
+            error: true,
+            message: "Session has expired, please login again!!"
+        };
+    }
+
+    try {
+        const imageNutrition = await prisma.imageNutrition.create({
+            data: {
+                idUser: verify.idUser,
+                idMakanan,
+                NamaAktivitas: namaAktivitas,
+                gambar: linkGambar,
+                waktuMakan,
+                porsi,
+            }
+        });
+
+        return {
+            status: 201,
+            error: false,
+            message: "Image nutrition data has been saved",
+            data: imageNutrition
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            error: true,
+            message: 'Failed to save image nutrition data',
+            data: error.message
+        };
+    }
+};
+
+const getFoodStorage = async (data) => {
+    const { token } = data;
+
+    const verify = verifyToken(token);
+
+    if (!verify) {
+        return {
+            status: 401,
+            error: true,
+            message: "Session has expired, please login again!!"
+        };
+    }
+
+    try {
+        const foodStorage = await prisma.foodStorage.findMany();
+
+        if (foodStorage.length === 0) {
+            return {
+                status: 404,
+                error: false,
+                message: "Food storage not found",
+                data: []
+            };
+        }
+
+        return {
+            status: 200,
+            error: false,
+            message: "Food storage data has been found",
+            data: foodStorage
+        };
+    } catch (error) {
+        return {
+            status: 500,
+            error: true,
+            message: 'Failed to retrieve food storage data',
+            data: error.message
+        };
+    }
+};
 
 module.exports = {
     getData,
-    detailData
-}
+    detailData,
+    saveImageNutrition,
+    getFoodStorage
+};
